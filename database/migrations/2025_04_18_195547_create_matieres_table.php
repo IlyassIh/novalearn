ff7d407891e5c8e42c98b53fd87a7c625773e60c
el-boussoughti <elboussoughti.med@gmail.com>
2025-04-19 03:20:51 +0100

    Updated migration files, (add matieres data)

--- database/migrations/2025_04_18_195547_create_matieres_table.php
+++ database/migrations/2025_04_18_195547_create_matieres_table.php
@@ -18,21 +18,99 @@ return new class extends Migration
         });
 
         DB::table('matieres')->insert([
-            ['nom' => 'Algorithmique et programmation 1'], //1
+            ['nom' => 'Algorithmique & Programmation 1'], //1
 
-            ['nom' => 'Systeme d\'exploitation 1'], //2
+            ['nom' => 'Systèmes d’exploitation 1'], //2
 
-            ['nom' => 'Communication Francais'], //3
+            ['nom' => 'Communication écrite et orale'], //3
 
-            ['nom' => 'Anglais Professionnel'], //4
+            ['nom' => 'Anglais technique'], //4
 
-            ['nom' => 'Mathematiques Appliquee'], //5
+            ['nom' => 'Mathématiques appliquées'], //5
 
-            ['nom' => 'Base de donnees 1'], //6
+            ['nom' => 'Base de données 1'], //6
 
-            ['nom' => 'Base de donnees 2'], //7
+            ['nom' => 'Base de données 2'], //7
 
             ['nom' => 'Gestion de projet'], //8
+
+            ['nom' => 'Algorithmique & Programmation 2'], //9
+
+            ['nom' => 'Systèmes d’exploitation 2'], //10
+
+            ['nom' => 'Mathématiques pour l’informatique'], //11
+
+            ['nom' => 'Bureautique'], //12
+
+            ['nom' => 'Réseaux 1 (TCP/IP, OSI…)'], //13
+
+            ['nom' => 'Réseaux 2 (Routage, Cisco…)'], //14
+
+            ['nom' => 'Architecture des ordinateurs'], //15
+
+            ['nom' => 'Programmation Orientée Objet (Java)'], //16
+
+            ['nom' => 'Programmation Web 1'], //17
+
+            ['nom' => 'Programmation Mobile'], //18
+
+            ['nom' => 'UML / Modélisation'], //19
+
+            ['nom' => 'Web Fullstack'], //20
+
+            ['nom' => 'Programmation Mobile avancée'], //21
+
+            ['nom' => 'Design Patterns / Archi Logicielle'], //22
+
+            ['nom' => 'Sécurité Informatique'], //23
+
+            ['nom' => 'Introduction aux réseaux'], //24
+
+            ['nom' => 'Admin Systèmes 1 (Linux)'], //25
+
+            ['nom' => 'Sécurité réseaux 1'], //26
+
+            ['nom' => 'Télécom'], //27
+
+            ['nom' => 'Réseaux 3 (VPN, DNS…)'], //28
+
+            ['nom' => 'Admin Systèmes 2 (Windows)'], //29
+
+            ['nom' => 'Sécurité réseaux 2'], //30
+
+            ['nom' => 'Supervision / Monitoring'], //31
+
+            ['nom' => 'Virtualisation & Cloud'], //32
+
+            ['nom' => 'Design Graphique'], //33
+
+            ['nom' => 'Infographie 2D'], //34
+
+            ['nom' => 'Infographie 3D'], //35
+
+            ['nom' => 'Animation / Montage vidéo'], //36
+
+            ['nom' => 'Audio numérique'], //37
+
+            ['nom' => 'Droit d’auteur / Éthique'], //38
+
+            ['nom' => 'Dev Web Multimédia'], //39
+
+            ['nom' => 'Dev Mobile / Interactivité'], //40
+
+            ['nom' => 'Modélisation 3D avancée'], //41
+
+            ['nom' => 'VFX / Effets spéciaux'], //42
+
+            ['nom' => 'Portfolio / Projet'], //43
+
+            ['nom' => 'Game Dev (Unity / Unreal)'], //44
+
+            ['nom' => 'Design UI/UX'], //45
+
+            ['nom' => 'Contenu Digital / Pub'], //46
+            
+            ['nom' => 'Réalité virtuelle / AR'], //47
         ]);
     }
 
--- database/migrations/2025_04_18_215358_create_filiere_matieres_table.php
+++ database/migrations/2025_04_18_215358_create_filiere_matieres_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->id();
 
             $table->unsignedBigInteger('filiere_id');
-            $table->foreign('filiere_id')->references('id')->on('filieres');
+            $table->foreign('filiere_id')->references('id')->on('filiers');
 
             $table->unsignedBigInteger('matiere_id');
             $table->foreign('matiere_id')->references('id')->on('matieres');
@@ -156,6 +156,14 @@ return new class extends Migration
             ],
 
             [
+                'filiere_id' => 3,
+                'matiere_id' => 5,
+                'annee_scolaire' => 1,
+                'semaistre' => 'S1',
+                'coef' => 2
+            ],
+
+            [
                 'filiere_id' => 4,
                 'matiere_id' => 5,
                 'annee_scolaire' => 1,
@@ -214,6 +222,413 @@ return new class extends Migration
                 'matiere_id' => 8,
                 'annee_scolaire' => 2,
                 'semaistre' => 'S3',
+                'coef' => 2
+            ],
+
+            //------------------------------- M9
+            [
+                'filiere_id' => 1,
+                'matiere_id' => 9,
+                'annee_scolaire' => 1,
+                'semaistre' => 'S2',
+                'coef' => 4
+            ],
+            [
+                'filiere_id' => 2,
+                'matiere_id' => 9,
+                'annee_scolaire' => 1,
+                'semaistre' => 'S2',
+                'coef' => 4
+            ],
+            [
+                'filiere_id' => 3,
+                'matiere_id' => 9,
+                'annee_scolaire' => 1,
+                'semaistre' => 'S2',
+                'coef' => 3
+            ],
+
+            //------------------------------- M10
+            [
+                'filiere_id' => 1,
+                'matiere_id' => 10,
+                'annee_scolaire' => 1,
+                'semaistre' => 'S2',
+                'coef' => 3
+            ],
+            [
+                'filiere_id' => 2,
+                'matiere_id' => 10,
+                'annee_scolaire' => 1,
+                'semaistre' => 'S2',
+                'coef' => 3
+            ],
+
+            //------------------------------- M11
+            [
+                'filiere_id' => 1,
+                'matiere_id' => 11,
+                'annee_scolaire' => 1,
+                'semaistre' => 'S2',
+                'coef' => 2
+            ],
+            [
+                'filiere_id' => 2,
+                'matiere_id' => 11,
+                'annee_scolaire' => 1,
+                'semaistre' => 'S2',
+                'coef' => 2
+            ],
+
+            //------------------------------- M12
+            [
+                'filiere_id' => 1,
+                'matiere_id' => 12,
+                'annee_scolaire' => 1,
+                'semaistre' => 'S1',
+                'coef' => 1
+            ],
+            [
+                'filiere_id' => 4,
+                'matiere_id' => 12,
+                'annee_scolaire' => 1,
+                'semaistre' => 'S2',
+                'coef' => 2
+            ],
+
+            //------------------------------- M13
+            [
+                'filiere_id' => 1,
+                'matiere_id' => 13,
+                'annee_scolaire' => 2,
+                'semaistre' => 'S3',
+                'coef' => 2
+            ],
+            [
+                'filiere_id' => 2,
+                'matiere_id' => 13,
+                'annee_scolaire' => 1,
+                'semaistre' => 'S2',
+                'coef' => 3
+            ],
+
+            //------------------------------- M14
+            [
+                'filiere_id' => 1,
+                'matiere_id' => 14,
+                'annee_scolaire' => 2,
+                'semaistre' => 'S4',
+                'coef' => 2
+            ],
+            [
+                'filiere_id' => 2,
+                'matiere_id' => 14,
+                'annee_scolaire' => 2,
+                'semaistre' => 'S3',
+                'coef' => 3
+            ],
+
+            //------------------------------- M15
+            [
+                'filiere_id' => 1,
+                'matiere_id' => 15,
+                'annee_scolaire' => 1,
+                'semaistre' => 'S1',
+                'coef' => 3
+            ],
+            [
+                'filiere_id' => 2,
+                'matiere_id' => 15,
+                'annee_scolaire' => 1,
+                'semaistre' => 'S1',
+                'coef' => 3
+            ],
+
+            //------------------------------- M16
+            [
+                'filiere_id' => 1,
+                'matiere_id' => 16,
+                'annee_scolaire' => 1,
+                'semaistre' => 'S2',
+                'coef' => 3
+            ],
+
+            //------------------------------- M17
+            [
+                'filiere_id' => 1,
+                'matiere_id' => 17,
+                'annee_scolaire' => 2,
+                'semaistre' => 'S3',
+                'coef' => 3
+            ],
+
+            //------------------------------- M18
+            [
+                'filiere_id' => 1,
+                'matiere_id' => 18,
+                'annee_scolaire' => 2,
+                'semaistre' => 'S3',
+                'coef' => 3
+            ],
+
+            //------------------------------- M19
+            [
+                'filiere_id' => 1,
+                'matiere_id' => 19,
+                'annee_scolaire' => 2,
+                'semaistre' => 'S3',
+                'coef' => 2
+            ],
+
+            //------------------------------- M20
+            [
+                'filiere_id' => 1,
+                'matiere_id' => 20,
+                'annee_scolaire' => 2,
+                'semaistre' => 'S4',
+                'coef' => 4
+            ],
+
+            //------------------------------- M21
+            [
+                'filiere_id' => 1,
+                'matiere_id' => 21,
+                'annee_scolaire' => 2,
+                'semaistre' => 'S4',
+                'coef' => 3
+            ],
+
+            //------------------------------- M22
+            [
+                'filiere_id' => 1,
+                'matiere_id' => 22,
+                'annee_scolaire' => 2,
+                'semaistre' => 'S4',
+                'coef' => 3
+            ],
+
+            //------------------------------- M23
+            [
+                'filiere_id' => 1,
+                'matiere_id' => 23,
+                'annee_scolaire' => 2,
+                'semaistre' => 'S4',
+                'coef' => 2
+            ],
+
+            //------------------------------- M24
+            [
+                'filiere_id' => 2,
+                'matiere_id' => 24,
+                'annee_scolaire' => 1,
+                'semaistre' => 'S1',
+                'coef' => 2
+            ],
+
+            //------------------------------- M25
+            [
+                'filiere_id' => 2,
+                'matiere_id' => 25,
+                'annee_scolaire' => 2,
+                'semaistre' => 'S3',
+                'coef' => 3
+            ],
+
+            //------------------------------- M26
+            [
+                'filiere_id' => 2,
+                'matiere_id' => 26,
+                'annee_scolaire' => 2,
+                'semaistre' => 'S3',
+                'coef' => 2
+            ],
+
+            //------------------------------- M27
+            [
+                'filiere_id' => 2,
+                'matiere_id' => 27,
+                'annee_scolaire' => 2,
+                'semaistre' => 'S3',
+                'coef' => 2
+            ],
+
+            //------------------------------- M28
+            [
+                'filiere_id' => 2,
+                'matiere_id' => 28,
+                'annee_scolaire' => 2,
+                'semaistre' => 'S4',
+                'coef' => 3
+            ],
+
+            //------------------------------- M29
+            [
+                'filiere_id' => 2,
+                'matiere_id' => 29,
+                'annee_scolaire' => 2,
+                'semaistre' => 'S4',
+                'coef' => 3
+            ],
+
+            //------------------------------- M30
+            [
+                'filiere_id' => 2,
+                'matiere_id' => 30,
+                'annee_scolaire' => 2,
+                'semaistre' => 'S4',
+                'coef' => 2
+            ],
+
+            //------------------------------- M31
+            [
+                'filiere_id' => 2,
+                'matiere_id' => 31,
+                'annee_scolaire' => 2,
+                'semaistre' => 'S4',
+                'coef' => 2
+            ],
+
+            //------------------------------- M32
+            [
+                'filiere_id' => 2,
+                'matiere_id' => 32,
+                'annee_scolaire' => 2,
+                'semaistre' => 'S4',
+                'coef' => 2
+            ],
+
+            //------------------------------- M33
+            [
+                'filiere_id' => 3,
+                'matiere_id' => 33,
+                'annee_scolaire' => 1,
+                'semaistre' => 'S1',
+                'coef' => 3
+            ],
+
+            //------------------------------- M34
+            [
+                'filiere_id' => 3,
+                'matiere_id' => 34,
+                'annee_scolaire' => 1,
+                'semaistre' => 'S1',
+                'coef' => 3
+            ],
+
+            //------------------------------- M35
+            [
+                'filiere_id' => 3,
+                'matiere_id' => 35,
+                'annee_scolaire' => 1,
+                'semaistre' => 'S2',
+                'coef' => 3
+            ],
+
+            //------------------------------- M36
+            [
+                'filiere_id' => 3,
+                'matiere_id' => 36,
+                'annee_scolaire' => 1,
+                'semaistre' => 'S2',
+                'coef' => 3
+            ],
+
+            //------------------------------- M37
+            [
+                'filiere_id' => 3,
+                'matiere_id' => 37,
+                'annee_scolaire' => 1,
+                'semaistre' => 'S2',
+                'coef' => 2
+            ],
+
+            //------------------------------- M38
+            [
+                'filiere_id' => 3,
+                'matiere_id' => 38,
+                'annee_scolaire' => 1,
+                'semaistre' => 'S2',
+                'coef' => 2
+            ],
+
+            //------------------------------- M39
+            [
+                'filiere_id' => 3,
+                'matiere_id' => 39,
+                'annee_scolaire' => 2,
+                'semaistre' => 'S3',
+                'coef' => 3
+            ],
+
+            //------------------------------- M40
+            [
+                'filiere_id' => 3,
+                'matiere_id' => 40,
+                'annee_scolaire' => 2,
+                'semaistre' => 'S3',
+                'coef' => 3
+            ],
+
+            //------------------------------- M41
+            [
+                'filiere_id' => 3,
+                'matiere_id' => 41,
+                'annee_scolaire' => 1,
+                'semaistre' => 'S3',
+                'coef' => 3
+            ],
+
+            //------------------------------- M42
+            [
+                'filiere_id' => 3,
+                'matiere_id' => 42,
+                'annee_scolaire' => 2,
+                'semaistre' => 'S3',
+                'coef' => 2
+            ],
+
+            //------------------------------- M43
+            [
+                'filiere_id' => 3,
+                'matiere_id' => 43,
+                'annee_scolaire' => 2,
+                'semaistre' => 'S3',
+                'coef' => 2
+            ],
+
+            //------------------------------- M44
+            [
+                'filiere_id' => 3,
+                'matiere_id' => 44,
+                'annee_scolaire' => 2,
+                'semaistre' => 'S4',
+                'coef' => 3
+            ],
+
+            //------------------------------- M45
+            [
+                'filiere_id' => 3,
+                'matiere_id' => 45,
+                'annee_scolaire' => 2,
+                'semaistre' => 'S4',
+                'coef' => 3
+            ],
+
+            //------------------------------- M46
+            [
+                'filiere_id' => 3,
+                'matiere_id' => 46,
+                'annee_scolaire' => 2,
+                'semaistre' => 'S4',
+                'coef' => 3
+            ],
+
+            //------------------------------- M47
+            [
+                'filiere_id' => 3,
+                'matiere_id' => 47,
+                'annee_scolaire' => 2,
+                'semaistre' => 'S4',
                 'coef' => 2
             ],
         ]);
